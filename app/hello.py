4c5323c49d5a3312e26b1d7027d802c1a8f4b61e
rss5799 <rss5799@psu.edu>
2025-05-31 18:28:44 -0400

    didn't get 100%...I don't think it's that important on this file

--- app/hello.py
+++ app/hello.py
@@ -1,8 +1,5 @@
 def say_hello():
     return f"Hello from the dev environment!"
 
-def main():
-    print(say_hello())
-
 if __name__ == "__main__":
-    main()
+    print(say_hello())
--- tests/test_hello.py
+++ tests/test_hello.py
@@ -1,6 +1,6 @@
 # This test is for the hello_test.py file.
 
-from app.hello import hello
+from app.hello import say_hello
 
 def test_say_hello():
-    assert hello.say_hello("Ryan") == "Hello from the dev environment!"
+    assert say_hello() == "Hello from the dev environment!"
