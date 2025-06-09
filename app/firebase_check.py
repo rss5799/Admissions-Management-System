ddcc8d2246ca09a9064593d3c8a2af40bcefab64
Tara Can <taracan@Taras-MacBook-Air.local>
2025-06-09 09:33:31 -0500

    transfer from firebase to student details page

--- app/firebase_check.py
+++ app/firebase_check.py
@@ -11,36 +11,15 @@ firebase_admin.initialize_app(cred)
 # Access Firestore
 db = firestore.client()
 
-# Firestore test
-print("Fetching students...")
-students_ref = db.collection("students")
-docs = students_ref.stream()
-
-empty = True
-for doc in docs:
-    empty = False
-    print(f"Found student: {doc.id} → {doc.to_dict()}")
-
-if empty:
-    print("No students found in database.")
-
-# Firebase Auth test
-print("\nFetching first 10 users from Firebase Auth...")
-page = auth.list_users(max_results=10)
-for user in page.users:
-    print(f"User: {user.uid} | Email: {user.email}")
-
-
-print("\nFirebase connection successful.")
 
 def upload_csv_to_firestore():
     try:
-        df = pd.read_csv("/Users/taracan/Documents/SWENG894/Admissions-Management-System/DummyData.csv")
+        df = pd.read_csv("/Users/taracan/Documents/SWENG894/Admissions-Management-System/DummyDataLite.csv")
     except FileNotFoundError:
         print(f"Error:CSV file not found at {csv_file_path}")
         return
     for index, row in df.iterrows():
-        doc_ref = db.collection("student_rc_and_test_scores").document()
+        doc_ref = db.collection("student").document()
         doc_ref.set(row.to_dict())
     print("Data uploaded successfully!")
 
