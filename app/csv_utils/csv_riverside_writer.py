5177c347c52af7884747bf566ac93249c6d66668
Tara Can <taracan@Taras-MacBook-Air.local>
2025-06-27 12:20:50 -0500

    tests for riverside merge to schoolmint csv

--- app/csv_utils/csv_riverside_writer.py
+++ app/csv_utils/csv_riverside_writer.py
@@ -7,31 +7,28 @@ import json
 import app
 
 
-MATRIX_FILE = 'app/services/admissions_matrix.json'
-with open(MATRIX_FILE, "r") as f:
-    matrix = json.load(f)
-
-
 #this function should be called when users press the button requesting that riverside data be merged with the most recent schoolmint csv
-def place_riverside_into_schoolmint():
+def place_riverside_into_schoolmint(schoolmintData, riversideResults):
+    
+    MATRIX_FILE = 'app/services/admissions_matrix.json'
+    with open(MATRIX_FILE, "r") as f:
+        matrix = json.load(f)
 
-    #pull the schoolmint file that's dropped into the system
-    original_schoolmint_data_no_tests = ('SchoolMintNoTests.csv')
-    with open(original_schoolmint_data_no_tests, 'r', newline='') as csv_file:
-        csv_reader = csv.reader(csv_file)
-        header = next(csv_reader)  # Reads the first row, which is the header
-        print(header)
-    print(original_schoolmint_data_no_tests[0])
-    #pull the riverside data that's dropped into the system
-    original_riverside_data = ('DummyRiversideData.csv')
+    #schoolMintData and riversideResults will be pulled from the file browser import page
 
     #convert both to dataframes
-    school_mint_df = pd.read_csv(original_schoolmint_data_no_tests)
-    riverside_df = pd.read_csv(original_riverside_data)
+    school_mint_df = pd.read_csv(schoolmintData)
+    riverside_df = pd.read_csv(riversideResults)
+
+    school_mint_df['id'].astype(str)
+    riverside_df['STUDENT ID 1'].astype(str)
 
     #join and drop nan values
-    merged_results = pd.merge(school_mint_df, riverside_df, left_on='id', right_on='STUDENT ID 1')    
+    merged_results = pd.merge(school_mint_df, riverside_df, left_on='id', right_on='STUDENT ID 1', how = 'outer')    
     merged_results = merged_results.fillna("")
+
+    #used to count the number of students whose records were updated (can be displayed to system user)
+    counter = 0
 
     #iterate down each row in the dataframe
     for index, row in merged_results.iterrows():
@@ -58,6 +55,7 @@ def place_riverside_into_schoolmint():
                 merged_results.loc[index, 'math_test_scores'] = row['MATH TOTAL - NPR']
                 merged_results.loc[index, 'matrix_math'] = math_matrix
             
+            counter += 1
             merged_results.loc[index, 'total_points'] = matrix_gpa + reading_matrix + language_matrix + math_matrix
 
         #update values and move to columns (second columns if it's the retest...)
@@ -75,11 +73,14 @@ def place_riverside_into_schoolmint():
                 merged_results.loc[index, 'math_test_scores2'] = row['MATH TOTAL - NPR']
                 merged_results.loc[index, 'matrix_math_retest'] = math_matrix
 
-            
+            counter += 1
             merged_results.loc[index, 'total_points_retest'] = matrix_gpa + reading_matrix + language_matrix + math_matrix
 
     headers = ['id', 'matrix_gpa', 'language_test_scores', 'reading_test_score', 'math_test_scores', 'total_points', 'matrix_languauge', 'matrix_math', 'matrix_reading', 'status', 'matrix_languauge_retest', 'matrix_math_retest', 'matrix_reading_restest', 'total_points_retest', 'updated_at', 'guardian1_email', 'guardian2_email', 'grade', 'deliver_test_accomodation_approved', 'test_date_sign_up', 'current_school', 'gpa', 'language_test_scores2', 'reading_test_score2', 'math_test_scores2']
-    merged_results.to_csv('SchoolMintNoTests.csv', columns = headers)
+    merged_results.to_csv(schoolmintData, columns = headers)
+
+    return counter
+
 
 
 
