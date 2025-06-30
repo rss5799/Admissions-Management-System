444055a87e47cd24071f46ba9fb579d681d89410
Tara Can <taracan@Taras-MacBook-Air.local>
2025-06-30 16:26:04 -0500

    working calculations and display (includes SCRUM-241)

---
+++ app/models/testday.py
@@ -0,0 +1,23 @@
+class Testday:
+    def __init__(self, date, firststandard8, firststandard9, firststandard10, firstaccomms8, firstaccomms9, firstaccomms10, reteststandard8, reteststandard9, reteststandard10, retestaccomms8, retestaccomms9, retestaccomms10, totalstandard, totalstandardrooms, totalaccomms, totalaccommsrooms, totalfirsttesters, totalretesters, totalstudents, totalrooms):
+        self.date = date
+        self.firststandard8 = firststandard8
+        self.firststandard9 = firststandard9
+        self.firststandard10 = firststandard10
+        self.firstaccomms8 = firstaccomms8
+        self.firstaccomms9 = firstaccomms9
+        self.firstaccomms10 = firstaccomms10
+        self.reteststandard8 = reteststandard8
+        self.reteststandard9 = reteststandard9
+        self.reteststandard10 = reteststandard10
+        self.retestaccomms8 = retestaccomms8
+        self.retestaccomms9 = retestaccomms9
+        self.retestaccomms10 = retestaccomms10
+        self.totalstandard = totalstandard
+        self.totalstandardrooms = totalstandardrooms
+        self.totalaccomms = totalaccomms
+        self.totalaccommsrooms = totalaccommsrooms
+        self.totalfirsttesters = totalfirsttesters
+        self.totalretesters = totalretesters
+        self.totalstudents = totalstudents
+        self.totalrooms = totalrooms
--- app/services/test_day_details.py
+++ app/services/test_day_details.py
@@ -1,4 +1,6 @@
 import pandas as pd
+from app.models.testday import Testday
+import math
 
 def retrieve_unique_test_dates(schoolMintData:str):
     upcoming_test_dates = []
@@ -14,9 +16,61 @@ def retrieve_unique_test_dates(schoolMintData:str):
     return upcoming_test_dates
 
 
+def retrieve_test_day_counts(schoolMintData:str, testday):
+    df_schoolmint = pd.read_csv(schoolMintData)
+    df_schoolmint = df_schoolmint.fillna("")
+    test_day_deets = Testday(testday, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
+    for index, row in df_schoolmint.iterrows():
+        if row['test_date_sign_up'] == testday:
+            grade = str(row['grade'])
+            accommsEligibility = str(row['deliver_test_accomodation_approved'])
+            #if grade 8
+            if grade == "9":
+                #if first test
+                if (row['language_test_scores'] == "" and row['reading_test_score'] == ""  and row['math_test_scores'] == ""):
+                    #if standard time
+                    if accommsEligibility != "Test Accommodations Approved":
+                        test_day_deets.firststandard8 += 1                    
+                    else:
+                        test_day_deets.firstaccomms8 +=1
+                else:
+                    if accommsEligibility != "Test Accommodations Approved":
+                        test_day_deets.reteststandard8 +=1
+                    else:
+                        test_day_deets.retestaccomms8 +=1
+            elif grade == "10":
+                #if first test
+                if (row['language_test_scores'] == "" and row['reading_test_score'] == ""  and row['math_test_scores'] == ""):
+                    #if standard time
+                    if accommsEligibility != "Test Accommodations Approved":
+                        test_day_deets.firststandard9 += 1                    
+                    else:
+                        test_day_deets.firstaccomms9 += 1                    
+                else:
+                    if accommsEligibility != "Test Accommodations Approved":
+                        test_day_deets.reteststandard9 += 1                    
+                    else:
+                        test_day_deets.retestaccomms9 += 1                    
+            else:
+                #if first test
+                if (row['language_test_scores'] == "" and row['reading_test_score'] == ""  and row['math_test_scores'] == ""):
+                    #if standard time
+                    if accommsEligibility != "Test Accommodations Approved":
+                        test_day_deets.firststandard10 += 1 
+                    else:
+                        test_day_deets.firstaccomms10 += 1 
+                else:
+                    if accommsEligibility != "Test Accommodations Approved":
+                        test_day_deets.reteststandard10 += 1 
+                    else:
+                        test_day_deets.retestaccomms10 += 1 
+    test_day_deets.totalstandard = test_day_deets.firststandard8 + test_day_deets.firststandard9 + test_day_deets.firststandard10 + test_day_deets.reteststandard8 + test_day_deets.reteststandard9 + test_day_deets.reteststandard10
+    test_day_deets.totalstandardrooms = math.ceil(test_day_deets.totalstandard / 25)
+    test_day_deets.totalaccomms = test_day_deets.firstaccomms8 + test_day_deets.firstaccomms9 + test_day_deets.firstaccomms10 + test_day_deets.retestaccomms8 + test_day_deets.retestaccomms9 + test_day_deets.retestaccomms10
+    test_day_deets.totalaccommsrooms = math.ceil(test_day_deets.totalaccomms / 25)
+    test_day_deets.totalfirsttesters = test_day_deets.firststandard8 + test_day_deets.firststandard9 + test_day_deets.firststandard10 + test_day_deets.firstaccomms8 + test_day_deets.firstaccomms9 + test_day_deets.firstaccomms10
+    test_day_deets.totalretesters = test_day_deets.reteststandard8 + test_day_deets.reteststandard9 + test_day_deets.reteststandard10 + test_day_deets.retestaccomms8 + test_day_deets.retestaccomms9 + test_day_deets.retestaccomms10
+    test_day_deets.totalstudents = test_day_deets.totalfirsttesters + test_day_deets.totalretesters
+    test_day_deets.totalrooms = test_day_deets.totalstandardrooms + test_day_deets.totalaccommsrooms
 
-#load the current csv
-#iterate down rows
-#if not accoms get 8th, 9th, 10th
-#if accoms get 8th, 9th, 10th
-#return dictionary {test_date, standard8, standard9, standard10, accomms8, accomms9, accomms10, total, rooms}
+    return test_day_deets
